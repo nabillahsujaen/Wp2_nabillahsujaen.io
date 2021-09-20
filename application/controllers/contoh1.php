d22cabc9beff1b4ed4963ff09f48afc07127c94b
nabillahsujaen <90845510+nabillahsujaen@users.noreply.github.com>
2021-09-20 09:10:47 -0700

    Add files via upload

---
+++ application/controllers/contoh1.php
@@ -0,0 +1,11 @@
+<?php
+class contoh1 extends CI_controller
+{
+    public function index()
+    {
+        echo "<h1>Perkenalkan<h1>";
+        echo"<h3>Nama saya Nabillah Sujaen,
+        <br>saya tinggal di Bekasi
+        <br>olahraga yang saya sukai adalah basket<h3>";
+    }
+}
